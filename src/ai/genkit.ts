3b7c7cceeb8de9284510d73dc779dcb1d48bca2c
Jio Baba <jiobaba369@gmail.com>
2025-07-09 04:14:47 +0000

    make BYD.Bio Ready to Publish?

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,9 +3,6 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-      // You may need to specify the API version for certain features
-      // apiVersion: 'v1beta',
-    }),
+    googleAI(),
   ],
 });
