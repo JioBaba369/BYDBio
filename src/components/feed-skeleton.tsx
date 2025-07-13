529758c1df9946d29b5705cd6a4b520f21fe2dab
Jio Baba <jiobaba369@gmail.com>
2025-07-13 07:12:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/feed-skeleton.tsx
+++ src/components/feed-skeleton.tsx
@@ -1,5 +1,5 @@
 
-import { Card, CardContent, CardHeader, CardFooter } from "@/components/ui/card";
+import { Card, CardHeader, CardFooter } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export const FeedSkeleton = () => (
@@ -45,7 +45,6 @@ export const FeedSkeleton = () => (
                     </div>
                 </div>
             </Card>
-        </Card>
         ))}
     </div>
 );
