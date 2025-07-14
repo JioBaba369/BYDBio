8c7f3a403b4212e8d85af2178892089aea2cc8a6
Jio Baba <jiobaba369@gmail.com>
2025-07-14 14:38:22 +0000

    improve toast

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,3 +1,4 @@
+
 "use client"
 
 // Inspired by react-hot-toast library
@@ -8,8 +9,8 @@ import type {
   ToastProps,
 } from "@/components/ui/toast"
 
-const TOAST_LIMIT = 1
-const TOAST_REMOVE_DELAY = 1000000
+const TOAST_LIMIT = 3
+const TOAST_REMOVE_DELAY = 5000
 
 type ToasterToast = ToastProps & {
   id: string
@@ -142,8 +143,9 @@ function dispatch(action: Action) {
 
 type Toast = Omit<ToasterToast, "id">
 
-function toast({ ...props }: Toast) {
+function toast(props: Toast) {
   const id = genId()
+  const duration = props.duration ?? TOAST_REMOVE_DELAY;
 
   const update = (props: ToasterToast) =>
     dispatch({
@@ -163,6 +165,10 @@ function toast({ ...props }: Toast) {
       },
     },
   })
+  
+  setTimeout(() => {
+    dismiss();
+  }, duration);
 
   return {
     id: id,
