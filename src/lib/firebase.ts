e3871aa5ddc1a8985eca77851e8f3352515bad49
Jio Baba <jiobaba369@gmail.com>
2025-07-16 00:05:37 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,21 +7,14 @@ import { getStorage } from "firebase/storage";
 import { getRemoteConfig } from "firebase/remote-config";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCPFwbrRFZTByiJT6ZcG--jdNxiR1GtwoA",
+  authDomain: "bydbio-dyowj.firebaseapp.com",
+  projectId: "bydbio-dyowj",
+  storageBucket: "bydbio-dyowj.appspot.com",
+  messagingSenderId: "454797679253",
+  appId: "1:454797679253:web:22fda57bfbd62dc93c999c",
+  measurementId: "G-P49NLQ126L"
 };
-
-// Check for missing Firebase configuration
-if (!firebaseConfig.apiKey) {
-    throw new Error(
-        'Firebase API Key is missing or invalid. Please check your .env file and ensure that all NEXT_PUBLIC_FIREBASE_* variables are set correctly. You can find these values in your Firebase project settings.'
-    );
-}
 
 
 // Initialize Firebase App (Singleton Pattern for Next.js)
